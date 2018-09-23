d158c952ea0695d1346cd568e11d11e1fc2299c7
GeoSyah <43498342+GeoSyah@users.noreply.github.com>
2018-09-23 14:11:11 +0700

    Add files via upload

---
+++ src/PBO2_10117081_Latihan8_Pecahan.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Geo Syah Alkautsar
+ * 10117081 IF-2
+ * Menampilkan cara penulisan bilangan pecahan di Java
+ */
+public class PBO2_10117081_Latihan8_Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      float f;
+      double d;
+      f = 1234567890.123456789F;
+      d = 1_234_567_890.123456789;
+      System.out.println("Float : " + f);
+      System.out.println("Double" + d);
+        
+    }
+    
+}
